74cc07e7850a0e29b1e746ec948310a0ef1e7a1f
강호경 <bandb20020301@gmail.com>
2026-02-24 18:49:50 +0900

    오늘의 백준

--- calculate.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-int main()
-{
-    int a;
-    int b;
-    std::cin >> a;
-    std::cin >> b;
-    std::cout << a + b << std::endl; // 주석입니다.
-}
---
+++ tailand.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main()
+{
+    int year;
+    std::cin >> year;
+    std::cout << year - 543 << std::endl;
+    return 0;
+}
